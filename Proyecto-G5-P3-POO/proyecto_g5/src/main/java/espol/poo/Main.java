7f41052a7e2b6457da05fde9a3e32a84c6c2256a
Sergtumb <gamer.anime.sath@gmail.com>
2025-11-11 18:47:22 -0500

    TecnicasEnfoque

--- Proyecto-G5-P3-POO/proyecto_g5/src/main/java/espol/poo/Main.java
+++ Proyecto-G5-P3-POO/proyecto_g5/src/main/java/espol/poo/Main.java
@@ -1,9 +1,12 @@
 package espol.poo;
 import espol.poo.controlador.ControladorPrincipal;
+import espol.poo.modelo.TecnicasEnfoque;
 
 public class Main {
     public static void main(String[] args) {
         ControladorPrincipal controlador = new ControladorPrincipal();
         controlador.iniciarMenuPrincipal();
+        TecnicasEnfoque t = new TecnicasEnfoque();
+        t.mostrarMenu();
     }
 }
